bbb7c4f4411493af7a60845b419ede08c6016144
rachela1 <rachel_kaitlin01@hotmail.com>
2023-05-25 18:27:57 -0500

    edited index.html and style.css

---
+++ script.js
@@ -0,0 +1,4 @@
+var APIKey = "6742848ffbdd73f2b4abfa38fd707bd8"
+var city;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+fetch(var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;)
