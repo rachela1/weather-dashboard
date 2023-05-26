0605fe36ad45bb5d4d3d267a4128da5fd7d8f271
rachela1 <rachel_kaitlin01@hotmail.com>
2023-05-26 01:37:35 -0500

    added script for search button and current weather

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 var APIKey = "6742848ffbdd73f2b4abfa38fd707bd8"
 var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + "742848ffbdd73f2b4abfa38fd707bd8";
-fetch(var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + "742848ffbdd73f2b4abfa38fd707bd8";)
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + "APIke";
+// fetch(var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + "742848ffbdd73f2b4abfa38fd707bd8";)
+
+var searchSubmitHandler = function(event) {
+    event.preventDefault();
+    var cityName = $("#cityName").val().trim();
+    if(cityName) {
+        getCurrentWeather(cityName);
+        $("#cityName").val("");
+    } else {
+        alert("please eantera cityanme");
+    }
+};
+
+
+var getCurrentWeather = function(city) {
+    var APIUrl=  "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + "742848ffbdd73f2b4abfa38fd707bd8";
+    fetch(APIUrl)
+        .then(function(response){
+            if (response.ok){
+             response.json().then(function(data){
+                displayWeather(data);
+             });
+        }else {
+            alert("error:" + response.statusText);
+      }   
+    }) 
+};
