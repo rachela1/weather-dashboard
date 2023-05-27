c5cd37f5cb56f67a7db215abb9b1f08ab3f35707
rachela1 <rachel_kaitlin01@hotmail.com>
2023-05-26 21:25:04 -0500

    added 5th day card and info to cards

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 var APIKey = "6742848ffbdd73f2b4abfa38fd707bd8"
 var city;
-// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + "APIke";
+//var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + "APIke";
 // fetch(var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + "742848ffbdd73f2b4abfa38fd707bd8";)
 
-var searchSubmitHandler = function(event) {
+var searchSubmitHandler = function (event) {
     event.preventDefault();
     var cityName = $("#cityName").val().trim();
-    if(cityName) {
+    if (cityName) {
         getCurrentWeather(cityName);
         $("#cityName").val("");
     } else {
-        alert("please eantera cityanme");
+        alert("Please enter a name of a city.");
     }
 };
 
-
-var getCurrentWeather = function(city) {
-    var APIUrl=  "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + "742848ffbdd73f2b4abfa38fd707bd8";
+var getCurrentWeather = function (city) {
+    var APIUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + "742848ffbdd73f2b4abfa38fd707bd8";
     fetch(APIUrl)
-        .then(function(response){
-            if (response.ok){
-             response.json().then(function(data){
-                displayWeather(data);
-             });
-        }else {
-            alert("error:" + response.statusText);
-      }   
-    }) 
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    displayWeather(data);
+                });
+            } else {
+                alert("error:" + response.statusText);
+            }
+        })
 };
+
